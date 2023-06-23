61f7154c7a929063376a69700a5157114a80dc97
Konstantin Petrykin <kpetrykin@gmail.com>
2023-06-23 20:16:58 +0000

    Introducing decorator for diagnose methods

--- router_diagnoster.py
+++ router_diagnoster.py
@@ -14,77 +14,61 @@ class RouterDiagnoster(Node):
 
         self._router_monitor = router_monitor
 
-        self.declare_parameter('cpu_critical_level', 0.9)
+        self.declare_parameter('cpu_critical_level', 0.1)
         self._cpu_critical_level = self.get_parameter('cpu_critical_level')
 
         self.declare_parameter('free_memory_critical_level', 5.0)
         self._free_memory_critical_level = self.get_parameter(
             'free_memory_critical_level')
 
-    # def diagnose_method(self, status_updater, diag_name):
-    #     def decorator(func):
-    #         diag_status = diagnostic_msgs.msg.DiagnosticStatus.OK
-    #         diag_message = f'{diag_name} is OK'
-    #         try:
-    #             func(status_updater)
-    #         except Exception as e:
-    #             diag_status = diagnostic_msgs.msg.DiagnosticStatus.ERROR
-    #             diag_message = f'Error getting {diag_name} data: {e}'
+    # Decorator for diagnose methods
+    def diagnose_method(diag_name):
+        def decorate(func):
+            def inner(self, status_updater):
+                diag_status = diagnostic_msgs.msg.DiagnosticStatus.OK
+                diag_message = f'{diag_name} is OK'
+                try:
+                    status_updater, diag_status, diag_message = func(self, status_updater, diag_status, diag_message)
+                except Exception as e:
+                    diag_status = diagnostic_msgs.msg.DiagnosticStatus.ERROR
+                    diag_message = f'Error getting {diag_name} data: {e}'
 
-    #         status_updater.summary(
-    #             diag_status,
-    #             diag_message)
+                status_updater.summary(
+                    diag_status,
+                    diag_message)
 
-    #         return status_updater
-    #     return decorator
+                return status_updater
+            return inner
+        return decorate
 
-    def diagnose_cpu(self, status_updater):
-        diag_status = diagnostic_msgs.msg.DiagnosticStatus.OK
-        diag_message = 'CPU is OK'
-        try:
-            cpu_data = self._router_monitor.get_cpu()
+    @diagnose_method('CPU')
+    def diagnose_cpu(self, status_updater, diag_status, diag_message):
+        cpu_data = self._router_monitor.get_cpu()
 
-            status_updater.add('1 min average load', str(cpu_data[0]))
+        status_updater.add('1 min average load', str(cpu_data[0]))
 
-            status_updater.add('5 min average load', str(cpu_data[1]))
-            if float(cpu_data[1]) > self._cpu_critical_level.value:
-                diag_status = diagnostic_msgs.msg.DiagnosticStatus.WARN
-                diag_message = '5 min average cpu load is too high!'
+        status_updater.add('5 min average load', str(cpu_data[1]))
+        if float(cpu_data[1]) > self._cpu_critical_level.value:
+            diag_status = diagnostic_msgs.msg.DiagnosticStatus.WARN
+            diag_message = '5 min average cpu load is too high!'
 
-            status_updater.add('15 min average load', str(cpu_data[2]))
-            if float(cpu_data[2]) > self._cpu_critical_level.value:
-                diag_status = diagnostic_msgs.msg.DiagnosticStatus.ERROR
-                diag_message = '15 min average cpu load is too high!'
-
-        except Exception as e:
+        status_updater.add('15 min average load', str(cpu_data[2]))
+        if float(cpu_data[2]) > self._cpu_critical_level.value:
             diag_status = diagnostic_msgs.msg.DiagnosticStatus.ERROR
-            diag_message = f'Error getting CPU data: {e}'
+            diag_message = '15 min average cpu load is too high!'
 
-        status_updater.summary(
-            diag_status,
-            diag_message)
+        return status_updater, diag_status, diag_message
 
-        return status_updater
+    @diagnose_method('Memory')
+    def diagnose_memory(self, status_updater, diag_status, diag_message):
+        memory_data = self._router_monitor.get_memory()
+        status_updater.add('Free memory, %', str(memory_data))
 
-    def diagnose_memory(self, status_updater):
-        diag_status = diagnostic_msgs.msg.DiagnosticStatus.OK
-        diag_message = 'Memory is OK'
-        try:
-            memory_data = self._router_monitor.get_memory()
-            status_updater.add('Free memory, %', str(memory_data))
+        if float(memory_data) < self._free_memory_critical_level.value:
+            diag_status = diagnostic_msgs.msg.DiagnosticStatus.WARN
+            diag_message = 'Low level of free memory!'
 
-            if float(memory_data) < self._free_memory_critical_level.value:
-                diag_status = diagnostic_msgs.msg.DiagnosticStatus.WARN
-                diag_message = 'Low level of free memory!'
-        except Exception as e:
-            diag_status = diagnostic_msgs.msg.DiagnosticStatus.ERROR
-            diag_message = f'Error getting memory data: {e}'
-
-        status_updater.summary(
-            diag_status,
-            diag_message)
-
-        return status_updater
+        return status_updater, diag_status, diag_message
 
     def diagnose_modem1(self, status_updater):
         diag_status = diagnostic_msgs.msg.DiagnosticStatus.OK
@@ -140,27 +124,20 @@ class RouterDiagnoster(Node):
 
         return status_updater
 
-    def diagnose_interfaces(self, status_updater):
+    @diagnose_method('Interfaces')
+    def diagnose_interfaces(self, status_updater, diag_status, diag_message):
         diag_status = diagnostic_msgs.msg.DiagnosticStatus.ERROR
         diag_message = 'No one interface is online!'
-        try:
-            interfaces_data = self._router_monitor.get_interfaces()
-            for interface in interfaces_data:
-                if interface['interface'] not in ['lan', 'loopback']:
-                    status_updater.add(
-                        interface['interface'], str(interface['up']))
-                    if interface['up']:
-                        diag_status = diagnostic_msgs.msg.DiagnosticStatus.OK
-                        diag_message = 'Interfaces are OK'
-        except Exception as e:
-            diag_status = diagnostic_msgs.msg.DiagnosticStatus.ERROR
-            diag_message = f'Error getting interfaces data: {e}'
+        interfaces_data = self._router_monitor.get_interfaces()
+        for interface in interfaces_data:
+            if interface['interface'] not in ['lan', 'loopback']:
+                status_updater.add(
+                    interface['interface'], str(interface['up']))
+                if interface['up']:
+                    diag_status = diagnostic_msgs.msg.DiagnosticStatus.OK
+                    diag_message = 'Interfaces are OK'
 
-        status_updater.summary(
-            diag_status,
-            diag_message)
-
-        return status_updater
+        return status_updater, diag_status, diag_message
 
 
 def main(args=None):
